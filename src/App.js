1a638ec31bf341d9ed6f7f4e4c931b8e2c7308f2
Gábor Dávid <46353496+gAbordAvid@users.noreply.github.com>
2023-03-15 11:44:34 +0100

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import NavigationBar from './components/Navbar';
 import './style/app.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>asd</h1>
+    <div>
+      <NavigationBar />
     </div>
   );
 }
